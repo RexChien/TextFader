adad04be6205c83026d3ab2860187b93f0b10396
RexChien <121141653+RexChien@users.noreply.github.com>
2024-01-14 23:28:47 +0800

    Add files via upload

--- test.ps1
+++ test.ps1
@@ -1,6 +1,14 @@
+
+$rainbow = @("#f00", "#ff7f00", "#ff0", "#0f0", "#0ff", "#00f", "#8b00ff")
+$by_the_see = @("#447294", "#8fbcdb", "#f4d6bc")
+$rose = @("#ffbad2", "#f20056", "#c8cfb4")
+
+
+#rainbow: ["#f00", "#ff7f00", "#ff0", "#0f0", "#0ff", "#00f", "#8b00ff"]
+#by the see: ["#447294", "#8fbcdb", "#f4d6bc"]
 try {
     node -v >$null
-    node .\fader.js '{"colors": ["#FF0000", "#00FF00", "#0000FF"], "txt": "./test.txt", "type": "horizontal", "output": "console"}'
+    node .\fader.js "{`"colors`": $($rose | ConvertTo-Json), `"txt`": `"./test.txt`", `"type`": `"horizontal`", `"output`": `"console`"}"
 }
 catch {
     "NG"
